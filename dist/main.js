06b19da8f35bd89a5c8efdeecd4eddce984951e5
Diegoav87 <diegoabdov@gmail.com>
2021-01-17 19:46:09 -0600

    Made a hamburger menu

---
+++ dist/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const overlay = document.querySelector('.overlay');
+const sideMenu = document.querySelector('.side-menu');
+const sideLinks = document.querySelectorAll('.menu-list a');
+
+let menuOpen = false;
+
+
+menuBtn.addEventListener('click', () => {
+    if (menuOpen === false) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        overlay.style.display = 'block';
+        sideMenu.style.left = '70%';
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        overlay.style.display = 'none';
+        sideMenu.style.left = '100%';
+    }
+    
+})
+
+sideLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sideMenu.style.left = '100%';
+        overlay.style.display = 'none';
+        menuOpen = false;
+        menuBtn.classList.remove('open');
+    });
+})
