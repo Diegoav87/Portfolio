0da4d3427b97c290d0999882d7a4eb772f676a69
Diegoav87 <diegoabdov@gmail.com>
2021-02-23 19:12:30 -0600

    Started react

--- dist/main.js
+++ dist/main.js
@@ -1,31 +1,31 @@
-const menuBtn = document.querySelector('.menu-btn');
-const overlay = document.querySelector('.overlay');
-const sideMenu = document.querySelector('.side-menu');
-const sideLinks = document.querySelectorAll('.menu-list a');
+const menuBtn = document.querySelector(".menu-btn");
+const overlay = document.querySelector(".overlay");
+const sideMenu = document.querySelector(".side-menu");
+const sideLinks = document.querySelectorAll(".menu-list a");
 
 let menuOpen = false;
 
+// Listener for hamburger menu
+menuBtn.addEventListener("click", () => {
+  if (menuOpen === false) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    overlay.style.display = "block";
+    sideMenu.style.left = "65%";
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    overlay.style.display = "none";
+    sideMenu.style.left = "100%";
+  }
+});
 
-menuBtn.addEventListener('click', () => {
-    if (menuOpen === false) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-        overlay.style.display = 'block';
-        sideMenu.style.left = '65%';
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-        overlay.style.display = 'none';
-        sideMenu.style.left = '100%';
-    }
-    
-})
-
-sideLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        sideMenu.style.left = '100%';
-        overlay.style.display = 'none';
-        menuOpen = false;
-        menuBtn.classList.remove('open');
-    });
-})
+// open side menu
+sideLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    sideMenu.style.left = "100%";
+    overlay.style.display = "none";
+    menuOpen = false;
+    menuBtn.classList.remove("open");
+  });
+});
