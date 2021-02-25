26d57585c9bdd77c7789ee580ff00564060dbe85
Diegoav87 <diegoabdov@gmail.com>
2021-02-25 17:47:11 -0600

    Fixing

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -5,20 +5,23 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Title />
-      <About />
-      <Projects />
-      <Services />
-      <Contact />
-      <footer className="footer">
-        <p>&copy; 2021 Diego Abdo</p>
-      </footer>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="App">
+        <Navbar />
+        <Title />
+        <About />
+        <Projects />
+        <Services />
+        <Contact />
+        <footer className="footer">
+          <p>&copy; 2021 Diego Abdo</p>
+        </footer>
+      </div>
+    </Suspense>
   );
 }
 
